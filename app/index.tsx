572fa514dd63bbd5abc67d43a6310a0fe0686ad1
HadiMaalikk <Hadimaliyakkal6@gmail.com>
2025-10-05 22:56:50 +0530

    Basics

--- app/index.tsx
+++ app/index.tsx
@@ -1,16 +1,11 @@
 import { Text, View } from "react-native";
+import { Link } from "expo-router"; 
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-        
-      }}
-    >
-      <Text >Siapp - Self Improvement App</Text>
+    <View className = "flex-1 items-center justify-center bg-maingreen">
+      <Text className="text-green-500">Siapp - Self Improvement App</Text>
+      <Link href="/hi" className="text-blue-500 font-bold">Click me to BUY</Link>
     </View>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   // NOTE: Update this to include the paths to all files that contain Nativewind classes.
-  content: ["./App.tsx", "./components/**/*.{js,jsx,ts,tsx}"],
+  content: ["./App.tsx","./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
     extend: {
-      colors : {
-        primary: '#',
-        secondary: '#',
-        light: {
-          100 : '#'
-        }
+      colors: {
+        maingreen: "#072820",
+        high: "#FFA500",
+        mainblack: "#000000",
+        medium: "#808080",
+        successgreen: "#00FF00",
+
+
       }
     },
   },
   plugins: [],
-}
+};
